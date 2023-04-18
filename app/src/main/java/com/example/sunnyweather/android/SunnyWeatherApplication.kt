6ddd8a19873d67d6d84accb2253842dff4c35452
王昱霄 <ruixia3@xiaohongshu.com>
2023-04-18 16:22:26 +0800

    ..

--- app/src/main/java/com/example/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/example/sunnyweather/android/SunnyWeatherApplication.kt
@@ -1,4 +1,19 @@
 package com.example.sunnyweather.android
 
-class SunnyWeatherApplication {
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication :Application() {
+
+    companion object{
+        const val TOKEN="填入你申请到的令牌值"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context:Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context=applicationContext
+    }
 }
--- app/src/main/java/com/example/sunnyweather/android/logic/model/PlaceResponse.kt
+++ app/src/main/java/com/example/sunnyweather/android/logic/model/PlaceResponse.kt
@@ -1,2 +1,12 @@
 package com.example.sunnyweather.android.logic.model
 
+import android.location.Location
+import com.google.gson.annotations.SerializedName
+import java.net.Inet4Address
+
+
+data class PlaceResponse(val staus:String,val places:List<Place>)
+
+data class Place(val name:String, val location: Location, @SerializedName("formatted_address") val address: String)
+
+data class Location(val lng:String, val lat:String)
